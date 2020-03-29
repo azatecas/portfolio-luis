9c5e631d228656a98829772715f833002173996c
Luis PM <luisp237@gmail.com>
2020-03-28 21:40:54 -0400

    typing animation on header functional, Base start for rest of portfolio

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+// import './Pencil.css';
+import Title from './components/Title';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+    </div>
+  );
+}
+
+export default App;
