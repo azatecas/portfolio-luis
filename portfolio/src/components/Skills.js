f37d16e084b8e2a377e9d8638ed7c6a2f6581aeb
Luis PM <luisp237@gmail.com>
2020-03-30 05:12:20 -0400

    add tech stack and their respective logos

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -1,55 +1,89 @@
 import React from 'react';
-import SkillsCarousel from './SkillsCarousel';
-
-
-
-
+import html5 from '../img/html5.svg';
+import css3 from '../img/css.svg';
+import sass from '../img/sass.svg';
+import less from '../img/less.svg';
+import javascript from '../img/JavaScript.svg';
+import reactjs from '../img/reactjs.svg';
+import reduximg from '../img/redux.svg';
+import nodeimg from '../img/nodejs.svg';
+import sqlite from '../img/sqlite.svg';
+import postgres from '../img/postgresql.svg';
 
 const Skills = () => {
-    return (
-        
+    return(
 
         <div id="skills" className="skills">
             <h3>Skills</h3>
 
             <div className="ind-skills">
                 <div className="my-skills">
+                    <img src={html5}
+                        alt="HTML5"
+                        />
                     <h3>HTML5</h3>
                 </div>
                 <div className="my-skills">
+                    <img src={css3}
+                        alt="css3"
+                        />
                     <h3>CSS</h3>
                 </div>
                 <div className="my-skills">
+                <img src={sass}
+                        alt="sass"
+                        />
                     <h3>SASS</h3> 
                 </div>
                 <div className="my-skills">
+                    <img src={less}
+                        alt="less"
+                        />
                     <h3>LESS</h3>
                 </div>
                 <div className="my-skills"> 
+                    <img src={javascript}
+                        alt="javascript"
+                        />
                     <h3>JavaScript</h3> 
                 </div>
                 <div className="my-skills">
+                <img src={reactjs}
+                        alt="react"
+                        style={{width: '950px'}}
+
+                        />
                     <h3>React.JS:</h3> 
                 </div>
-
                 <div className="my-skills">
-                    <h3>Jest.JS</h3>
-                </div>
-
-                <div className="my-skills">
+                <img src={reduximg}
+                        alt="redux"
+                        />
                     <h3>Redux</h3>
                 </div>
                 <div className="my-skills">
+                <img src={nodeimg}
+                        alt="node"
+                        />
                     <h3>Node.js</h3>
                 </div>
                 <div className="my-skills">
-                    <h3>SQLite3 Database</h3>
+                <img src={sqlite}
+                        alt="sqlite"
+                        />
+                    <h3>SQLite3</h3>
+                </div>
+                <div className="my-skills">
+                <img src={postgres}
+                        alt="postgres"
+                        />
+                    <h3>PostgreSQL</h3>
                 </div>
 
             </div>
-        </div>
+        </div> 
     )
-}
 
+}
 
 export default Skills;
