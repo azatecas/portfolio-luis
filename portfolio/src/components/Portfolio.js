bd1dbeb6aa00283e36b06591d8af50e228fea452
Luis PM <luisp237@gmail.com>
2020-03-31 23:43:29 -0400

    fetch projects from backend and render them onscreen

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -1,44 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import porfolioimg from '../img/portfolio1.png';
+import Axios from 'axios';
 
 const Portfolio = () => {
+
+    const [ projects, setProjects ] = useState([]);
+    
+    useEffect(() => {
+        Axios
+            .get(`https://my-portfolio-luis.herokuapp.com/api/projects`)
+            .then(res => {
+                console.log(res.data);
+                setProjects(res.data);
+            })
+            .catch(err => console.log('error fetching projects', err))
+    }, []);
+
+
     return (
         <div id ='#projects' className='portfolio'>
-        <h3>Projects</h3>
-        <div className="portfolio-cont">
-            <div className="my-project">
-                <img src={porfolioimg} alt='portfolio-project' />
-                <div>
-                    <h4>portfolio</h4>
-                    <p>Laurem ipson Laurem ipson</p>
-                </div>
-            </div>
-
-            <div className="my-project">
-                <img src={porfolioimg} alt='portfolio-project' />
-                <div>
-                    <h4>portfolio</h4>
-                    <p>Laurem ipson Laurem ipson</p>
-                </div>
-            </div>
-
-            <div className="my-project">
-                <img src={porfolioimg} alt='portfolio-project' />
-                <div>
-                    <h4>portfolio</h4>
-                    <p>Laurem ipson Laurem ipson</p>
-                </div>
-            </div>
-            <div className="my-project">
-                <img src={porfolioimg} alt='portfolio-project' />
-                <div>
-                    <h4>portfolio</h4>
-                    <p>Laurem ipson Laurem ipson</p>
-                </div>
-            </div>
-
-            </div>
+            <h3>Projects</h3>
             
+            <div className="portfolio-cont" >
+            {projects.map(item => (                    
+                <div className="my-project" key={item.id}>
+                    <img src={item.img_url} alt={`${item.project_title} project`} />
+                    <div>
+                        <h4>{item.project_title}</h4>
+                        <p>{item.project_desc}</p>
+                    </div>
+                </div>                    
+            ))}
+            </div>
         </div>
     )
 }
